cc5c2c58a213252fa480f792188afa74379f3068
MariannePiquetNowak <mariannexpiquet@gmail.com>
2021-09-19 12:01:57 +0200

    Project main page ok

--- src/Components/Project/index.js
+++ src/Components/Project/index.js
@@ -24,12 +24,20 @@ let slides = [
 
 const Project = () => {
     return (
-        <Container className="Project">
-            <h1>Mes Projets</h1>
-            <span>Voici les différents projets sur lesquels j'ai été amené à travailler</span>
+        <Container className="Project text-center">
+            <div className="header-project mb-5">
+                <h1>Mes Projets</h1>
+                <h3>Voici les différents projets sur lesquels j'ai été amené à travailler</h3>
+            </div>
+            
             <Carousel slides={slides} autoplay={false} />
+            <Button className="button-project mb-3" variant="outline-primary">Voir tous les projets</Button>
         </Container>
     )
 }
 
 export default Project;
+
+// Version mobile : Retirer le slider et remplacer par une image simple ? 
+// Ajouter images de projet dans le slider
+// Créer une page Projects, ajouter le chemin au bouton
